84d04163c5261d8abc4a0380b301174654565677
FerryAchmadAkbar <167895970+FerryAchmadAkbar4522210088@users.noreply.github.com>
2025-03-23 23:46:50 +0700

    Update hafidz.dart

--- lib/pages/hafidz.dart
+++ lib/pages/hafidz.dart
@@ -1,27 +1,65 @@
 import 'package:flutter/material.dart';
+import 'home.dart'; // Pastikan ini sesuai dengan lokasi file HomePage
 
-class HafidzPage extends StatelessWidget {
+class HafidzPage extends StatefulWidget {
+  @override
+  _HafidzPageState createState() => _HafidzPageState();
+}
+
+class _HafidzPageState extends State<HafidzPage> {
+  int _selectedIndex = 1; // Atur indeks default ke halaman saat ini
+
+  void _onItemTapped(int index) {
+    if (index == 0) {
+      // Jika Home diklik, kembali ke HomePage
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => HomePage()));
+    }
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Hafidz")),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Nama: Muhammad Hafidz Aulia"),
-                Text("NPM: 4522210063"),
-              ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text("Nama: Muhammad Hafidz Aulia"),
+            Text("NPM: 4522210063"),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        margin: EdgeInsets.only(bottom: 30, left: 25, right: 25),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 2,
+              spreadRadius: 0.1,
             ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(30),
+          child: BottomNavigationBar(
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
+            selectedItemColor: Colors.blue,
+            unselectedItemColor: Colors.grey,
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+              BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+              BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+            ],
           ),
-          ElevatedButton(
-            onPressed: () => Navigator.pop(context),
-            child: const Text("Kembali"),
-          ),
-        ],
+        ),
       ),
     );
   }
